c16f103a2576ba24f7c43a6d0ff58451ce142a68
Samuel Ademujimi <samshotmedia01@gmail.com>
2023-09-12 05:05:00 +0100

    fix: connections.js

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,11 @@
-import dotenv from 'dotenv';
-dotenv.config();
+
 import mongoose from "mongoose";
 const databaseUrl = process.env.DATABASE_URI;
 
 
 async function connect() {
   mongoose.set("strictQuery", true);
-  const db = await mongoose.connect(databaseUrl);
+  const db = await mongoose.connect(process.env.DATABASE_URI);
   console.log("Database Connected at ", db.connection.host);
   return db;
 }
