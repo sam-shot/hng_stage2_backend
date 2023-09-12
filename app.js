3e3f95be25d72ff0761dd57c8a3ed9076dcea90e
Samuel Ademujimi <samshotmedia01@gmail.com>
2023-09-12 04:49:08 +0100

    fix: changed PORT

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000
 connect()
     .then(() => {
         try {
-            app.listen(PORT, function () {
+            app.listen(3000, function () {
                 console.log(`Server listening at port ${PORT}`);
             });
         } catch (error) {
